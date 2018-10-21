1b0a698ddaf3089da9fd0f22b68905f9dd829b8c
David Whynot <davidmwhynot@gmail.com>
2018-10-21 16:34:50 -0400

    finished video 8

--- src/app/index.js
+++ src/app/index.js
@@ -6,17 +6,11 @@ import { Home } from './components/Home';
 
 class App extends React.Component {
 	render() {
-		let user = {
-			name: 'Anna',
-			hobbies: ['Sports']
-		};
 		return(
 			<div>
 				<Header/>
 				<div className="container">
-					<Home name={'Max'} age={28} user={user}>
-						<p>This is a child!</p>
-					</Home>
+					<Home name={'Max'} initialAge={28}/>
 				</div>
 			</div>
 		);
