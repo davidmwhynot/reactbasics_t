61e0cf1183a35eb02b6d260a2d210cf07beddb3d
David Whynot <davidmwhynot@gmail.com>
2018-10-21 16:17:06 -0400

    finished video 6

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,26 @@
-console.log('it works');
+import React from 'react';
+import { render } from 'react-dom';
+
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
+class App extends React.Component {
+	render() {
+		let user = {
+			name: 'Anna',
+			hobbies: ['Sports']
+		};
+		return(
+			<div>
+				<Header/>
+				<div className="container">
+					<Home name={'Max'} age={28} user={user}>
+						<p>This is a child!</p>
+					</Home>
+				</div>
+			</div>
+		);
+	}
+}
+
+render(<App/>, $('#app')[0]);
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const DIST_DIR = path.resolve(__dirname, 'dist');
 const SRC_DIR = path.resolve(__dirname, 'src');
 
 const config = {
-	entry: SRC_DIR + '/app/index.js',
+	entry: path.join(SRC_DIR, 'app', 'index.js'),
 	output: {
-		path: DIST_DIR + '/app',
+		path: path.join(DIST_DIR, 'app'),
 		filename: 'bundle.js',
-		publicPath: '/app/'
+		publicPath: 'app'
 	},
-	modules: {
+	module: {
 		loaders: [
 			{
 				test: /\.js?/,
